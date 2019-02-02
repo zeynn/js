3b188aad11297e298675b24ab7a6db70fa2b4f41
zeynn <45535863+zeynn@users.noreply.github.com>
2019-02-02 17:58:26 +0300

    Add files via upload

---
+++ js/script.js
@@ -0,0 +1,52 @@
+function  ttt()
+{
+	var ahmet = true;
+	if (typeof ahmet!="undefined")
+	{
+		if(ahmet==true)
+		{
+			console.log("TRUEEEEE");
+		} else if(ahmet==false)
+		{
+			console.log("pinokyo");
+		} else if(ahmet==null)
+		{
+			console.log("bilinmeyen");
+		}
+		else
+		{
+			console.log("tanimlanmadi");
+		}
+	}
+	else
+	{
+		console.log("boyle bisey yok");
+	}
+}
+
+
+function fff()
+{
+	var ahmet =false;
+	if (typeof ahmet!="undefined")
+	{
+		if(ahmet==true)
+		{
+			console.log("dogru");
+		} else if(ahmet==false)
+		{
+			console.log("YOU ARE A LİAR");
+		} else if(ahmet==null)
+		{
+			console.log("bilinmeyen");
+		}
+		else
+		{
+			console.log("tanimlanmadi");
+		}
+	}
+	else
+	{
+		console.log("boyle bisey yok");
+	}
+}
